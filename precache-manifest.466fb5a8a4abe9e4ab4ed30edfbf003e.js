214525994f70944d875a957a1a3c94ca4dac230e
ebijvoet <60981169+ebijvoet@users.noreply.github.com>
2020-10-05 17:46:00 +0200

    Updates

---
+++ precache-manifest.466fb5a8a4abe9e4ab4ed30edfbf003e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579969227be3894dc1db2d447d4f7543",
+    "url": "/ebijvoet/simple-todo-app/index.html"
+  },
+  {
+    "revision": "b84f991fa9b400317f8d",
+    "url": "/ebijvoet/simple-todo-app/static/css/main.cfa66741.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/ebijvoet/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ebijvoet/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84f991fa9b400317f8d",
+    "url": "/ebijvoet/simple-todo-app/static/js/main.dfd98281.chunk.js"
+  },
+  {
+    "revision": "2fe1852a68889b210c8d",
+    "url": "/ebijvoet/simple-todo-app/static/js/runtime-main.8fdd3b89.js"
+  }
+]);
